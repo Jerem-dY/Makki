7ad090af7be08b4a489fb1f215164698446a11fb
Jerem-dY <jeremy.bourdillat.09@gmail.com>
2024-01-15 20:01:56 +0100

    proto api

---
+++ api/url.php
@@ -0,0 +1,124 @@
+<?php 
+
+class URLParser {
+
+    function __construct(string $config_file) {
+
+        //TODO: ajouter au constructeur un paramètre 'db'
+
+        $str = file_get_contents($config_file);
+
+        if ($str == false) {
+            echo "No config file";
+            $this->config = array();
+        }
+        else {
+
+            $config = json_decode($str, true); 
+
+            if(isset($config["pages"])) {
+                $this->pages = $config["pages"];
+            }
+            else {
+                echo "No 'pages' configuration.";
+            }
+        }
+
+        $this->langs = array("fr", "ar"); //TODO: A remplacer par une requête à la DB
+
+        //print_r($this->pages);
+        //print_r($this->langs); 
+    }
+
+    /**
+     * Cette méthode traite une uri et renvoie un tableau associatif des données qu'elle contient.
+     */
+    public function parse_uri(string $uri) {
+
+        $output = array();
+
+        // On récupère la différence entre le chemin complet (serveur compris) et le chemin d'accès du fichier php ; 
+        // cela permet de faire la différence entre les deux et de récupérer le chemin d'accès à la ressource seul,
+        // Permettant ainsi de ne pas nécessiter de connaissance a priori du nom du serveur (pratique pour migrer le site)
+        // Ex : 
+        // uri = 		"i3l.univ-grenoble-alpes.fr/~makki/fr/lexique"
+        // PHP_SELF = 	"i3l.univ-grenoble-alpes.fr/~makki/index.php"
+        // On récupère `dirname` de PHP_SELF, soit "i3l.univ-grenoble-alpes.fr/~makki"
+        // En faisant la différence, on obtient : "/fr/lexique"
+        $php_loc = pathinfo($_SERVER['PHP_SELF'])['dirname'];
+
+        $r = substr($uri, strlen($php_loc));
+
+        // On récupère les champs de requête (recherche, etc.) et le chemin vers la ressource
+        $query_s = parse_url($r, PHP_URL_QUERY);
+        $path = parse_url($r, PHP_URL_PATH);
+
+        // Si une query a été fournie, on la récupère sous la forme d'un tableau associatif
+        if ($query_s != null) {
+            $output['query'] = array();
+            parse_str($query_s, $output['query']);
+        }
+
+        // On récupère les éléments séparés par un slash '/', et on se débarasse du premier élément
+        $elements = explode("/", $path);
+        array_shift($elements);
+        $size = sizeof($elements);
+
+        if ($size == 0) {
+            $output['code'] = 404;
+        }
+
+        //TODO: remplacer par une requête à la DB pour récupérer les codes de toutes les langues gérées
+        if (!in_array($elements[0], $this->langs)) {
+            echo "BAD LANG: " . $elements[0];
+            $output['code'] = 404;
+        }
+        else {
+            $output['lang'] = $elements[0];
+        }
+
+        // Exemples d'urls valides :
+        // [lang]/[page]/[target]?[query]
+        // fr/lexique/أباجُور/
+        // fr/theme/commun
+        // fr/images/bebe.png
+        // fr/scripts/jquery.js
+        // fr/styles/style.css
+
+        if ($size > 1) {
+
+            if (isset($this->pages[$elements[1]])) {
+                $output['page'] = $this->pages[$elements[1]];
+                $output['type'] = $elements[1];
+            }
+            else {
+                echo "BAD PAGE: " . $elements[1];
+                $output['code'] = 404;
+            }
+
+            if ($size > 2) {
+        
+                $output['target'] = $elements[2];
+
+                // On accept un seul '/' traînant et pas d'autres éléments
+                // fr/lexique/أباجُور/		✅
+                // fr/lexique/أباجُور		✅
+                // fr/lexique/أباجُور//		❌
+                // fr/lexique/أباجُور/a/	❌
+                // fr/lexique/أباجُور/a/b	❌
+                if ($size == 4 && $elements[3] != '' || $size > 4) {
+                    echo "BAD TARGET: " . $elements[3];
+                    $output['code'] = 404;
+                }
+            }
+        }
+
+        if(!isset($output['code'])) {
+            $output['code'] = 200;
+        }
+
+        return $output;
+    }
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,128 @@
+<?php 
+
+require("api/url.php");
+require("api/simple_html_dom/simple_html_dom.php");
+
+
+header("Access-Control-Allow-Origin: *");
+
+// get request method
+$method = $_SERVER['REQUEST_METHOD'];
+
+if (isset($_SERVER["HTTP_CONTENT_TYPE"]) && $_SERVER["HTTP_CONTENT_TYPE"] != '') {
+	$content_type = $_SERVER["HTTP_CONTENT_TYPE"];
+}
+else {
+	$content_type = "text/html";
+}
+
+header('Content-Type: '.$content_type);
+
+//echo $content_type . "<br/>";
+
+$url_parser = new URLParser("config/url.json");
+$uri = $_SERVER['REQUEST_URI'];
+
+$request = $url_parser->parse_uri($uri);
+
+if ($request->code != 200) {
+	http_response_code(404);
+	exit;
+}
+
+if ($method == 'GET') {
+
+	// Si aucune page exacte est précisée ('accueil.html' par exemple) cela signifie que c'est une ressource type image/css/etc. (voir url.json)
+	if ($request['page'] == "") {
+		
+		if ($request['type'] == 'images') {
+
+			$img_path = "html/images/".$request['target'];
+
+			if (file_exists($img_path)) {
+				
+				header("Content-Type: ".image_type_to_mime_type(exif_imagetype($img_path)));
+				header("Content-Length: " . filesize($img_path));
+
+				$res = readfile($img_path);
+
+				if ($res == false) {
+					echo "ERROR";
+				}
+				exit;
+				
+			}
+			else {
+				http_response_code(404);
+			}
+			
+		}
+		else if ($request['type'] == 'styles') {
+
+			$css_path = "html/".$request['target'];
+
+			if (file_exists($css_path)) {
+				
+				header("Content-Type: text/css");
+				header("Content-Length: " . filesize($css_path));
+
+				$res = readfile($css_path);
+
+				if ($res == false) {
+					echo "ERROR";
+				}
+
+				exit;
+				
+			}
+			else {
+				http_response_code(404);
+			}
+			
+		}
+	}
+	else {
+		if ($content_type == "text/html") {
+			if (file_exists("html/".$request['page'])) {
+
+				$html = file_get_html("html/".$request['page']);
+
+				// On adapte les attributs de la page afin de permettre au navigateur de formuler la bonne requête :
+
+				// Images
+				foreach($html->find('img') as $e)
+    				$e->src = pathinfo($_SERVER['PHP_SELF'])['dirname'].'/'.$request['lang'].'/'.$e->src;
+				
+				foreach($html->find('link') as $e)
+
+				// Feuilles de style
+				if ($e->rel == "stylesheet") {
+					$e->href = pathinfo($_SERVER['PHP_SELF'])['dirname'].'/'.$request['lang'].'/styles/'.$e->href;
+				}
+
+				echo $html;
+			}
+			else {
+				echo "NO SUCH PAGE BITCH";
+			}
+		
+		}
+		else {
+			echo $content_type;
+		}
+	}
+}
+else if ($method == 'POST') {
+	echo "THIS IS A POST REQUEST";
+}
+else if ($method == 'PUT') {
+	echo "THIS IS A PUT REQUEST";
+}
+else if ($method == 'DELETE') {
+	echo "THIS IS A DELETE REQUEST";
+}
+else {
+	echo "WUT?!";
+}
+
+?>
